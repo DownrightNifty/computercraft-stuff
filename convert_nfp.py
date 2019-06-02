12a6ba88aed8782fb0e3d5e692f3f91a491de223
DownrightImpractical <51222679+DownrightImpractical@users.noreply.github.com>
2019-06-01 21:06:06 -0500

    Create convert_nfp.py

---
+++ convert_nfp.py
@@ -0,0 +1,63 @@
+from PIL import Image
+import nfp
+import argparse
+import os
+
+# default resize width/height when converting image -> nfp
+DEFAULT_WIDTH, DEFAULT_HEIGHT = 164, 81
+
+desc = (
+    "Convert standard image files to ComputerCraft nfp files, and vice "
+    "versa. Input file type is identified by extension (.nfp, .jpg, etc.), "
+    "and output files use the input filename with a new extension."
+)
+file_help = "input file, nfp or image (must have correct file extension)"
+nfp_desc = "optional arguments when converting image -> nfp"
+skip_help = "skip default behavior of resizing image before conversion"
+width_help = "if resizing, new width (default: {})".format(DEFAULT_WIDTH)
+height_help = "if resizing, new height (default: {})".format(DEFAULT_HEIGHT)
+im_desc = "optional arguments when converting nfp -> image"
+format_help = (
+    "output format passed to Image.save() (also output file extension "
+    "unless -e argument specified), see PIL docs for supported formats, "
+    "default: PNG"
+)
+ext_help = (
+    "if specified, will be used as the output file extension instead of "
+    "FORMAT"
+)
+
+parser = argparse.ArgumentParser(description=desc)
+parser.add_argument("file", help=file_help)
+nfp_group = parser.add_argument_group("nfp arguments", description=nfp_desc)
+nfp_group.add_argument("--skip-resize", "-s", help=skip_help,
+                       action="store_true", default=False)
+nfp_group.add_argument("--resize-width", "-w", help=width_help,
+                       metavar="WIDTH", type=int, default=DEFAULT_WIDTH)
+nfp_group.add_argument("--resize-height", "-H", help=height_help,
+                       metavar="HEIGHT", type=int, default=DEFAULT_HEIGHT)
+im_group = parser.add_argument_group("image arguments", description=im_desc)
+im_group.add_argument("--format", "-f", help=format_help, metavar="FORMAT",
+                      dest="f_format", default="PNG")
+im_group.add_argument("--extension", "-e", help=ext_help)
+
+args = parser.parse_args()
+filename, ext = os.path.splitext(args.file)
+if not ext:
+    parser.error("filename must have appropriate extension")
+if ext.upper() == ".NFP":
+    with open(args.file, "rt") as f:
+        nfp_file = f.read()
+    im = nfp.nfp_to_img(nfp_file)
+    new_ext = args.f_format.replace(" ", "").lower()
+    if args.extension:
+        new_ext = args.extension
+    im.save("{}.{}".format(filename, new_ext), args.f_format)
+else:
+    im = Image.open(args.file)
+    if args.skip_resize:
+        nfp_file = nfp.img_to_nfp(im)
+    else:
+        nfp_file = nfp.img_to_nfp(im, (args.resize_width, args.resize_height))
+    with open("{}.nfp".format(filename), "wt") as f:
+        f.write(nfp_file)
